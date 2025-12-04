42422fbcd18a4d2cd5e1419f7308bbaaf4262c7c
Qarib Hasan <qhasan.bscs25seecs@seecs.edu.pk>
2025-12-04 11:13:17 +0500

    Add new features file

---
+++ ew_feature.py
@@ -0,0 +1,7 @@
+def even_odd_check(num):
+    if num % 2 == 0:
+        print("Even Number")
+    else :
+        print("Odd Number")
+num = int(input("Enter a number: "))
+print(even_odd_check(num))
